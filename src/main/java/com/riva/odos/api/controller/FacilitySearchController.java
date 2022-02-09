dffbdc1f18cb05361887a6ebfca4a6c713970d4d
Greg <rebelliondevelopment@gmail.com>
2022-02-09 12:44:18 -0500

    greg-286: change it all

---
+++ src/main/java/com/riva/odos/api/controller/FacilitySearchController.java
@@ -0,0 +1,29 @@
+package com.riva.odos.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.riva.odos.domain.AirportInfoDto;
+import com.riva.odos.domain.FacilityDto;
+import com.riva.odos.services.FacilityService;
+
+@RestController
+@RequestMapping(value="/api/v1", produces= {MediaType.APPLICATION_JSON_VALUE})
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class FacilitySearchController {
+
+	@Autowired
+	FacilityService facilityService;
+	
+    @GetMapping(value="/facilitySearch")
+    public List<FacilityDto> facilitySearch(@RequestParam(value="searchZipCode") String searchZipCode) {
+        return facilityService.searchFacility(searchZipCode);
+    }
+}
